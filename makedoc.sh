e14aee8756c559ba5862ab18784845a6e1bc9f1a
gkholman <gkenholman+github@gmail.com>
2024-10-11 14:35:21 +0300

    #16 Produce documentation on server for release and update

--- makedoc.sh
+++ makedoc.sh
@@ -8,7 +8,44 @@ echo utilities/saxon655/ directory:
 echo https://sourceforge.net/projects/saxon/files/saxon6/6.5.5/
 exit 1
 fi
+
+if [ -f designSVG2burnFiles.html ]; then rm designSVG2burnFiles.html ; fi
+if [ -f convertBadStrokes4designSVG.html ]; then rm convertBadStrokes4designSVG.html ; fi
+
+echo Making designSVG2burnFiles.html
 java -jar utilities/saxon655/saxon.jar -a -o designSVG2burnFiles.html designSVG2burnFiles.xsl
-open designSVG2burnFiles.html
+if [ $? -ne 0 ]; then
+  echo Problem executing Saxon for designSVG2burnFiles.html
+  exit 1
+fi
+if [ ! -f designSVG2burnFiles.html ]; then
+  echo "Error: Documentation designSVG2burnFiles.html not produced"
+  exit 1
+fi
+grep -q -i "inconsistencies.detected" designSVG2burnFiles.html
+retdesign=$?
+if [ $retdesign -eq 0 ]; then
+  echo "Error: The file designSVG2burnFiles.html contains inconsistencies."
+fi
+
+echo Making convertBadStrokes4designSVG.html
 java -jar utilities/saxon655/saxon.jar -a -o convertBadStrokes4designSVG.html convertBadStrokes4designSVG.xsl
-open convertBadStrokes4designSVG.html
+if [ $? -ne 0 ]; then
+  echo Problem executing Saxon for convertBadStrokes4designSVG.html
+  exit 1
+fi
+if [ ! -f convertBadStrokes4designSVG.html ]; then
+  echo "Error: Documentation convertBadStrokes4designSVG.html not produced"
+  exit 1
+fi
+grep -q -i "inconsistencies.detected" convertBadStrokes4designSVG.html
+retconvert=$?
+if [ $retconvert -eq 0 ]; then
+  echo "Error: The file convertBadStrokes4designSVG.html contains inconsistencies."
+fi
+
+if [ $retconvert -eq 0 ] || [ $retdesign -eq 0 ]; then
+  exit 1
+fi
+
+echo Documentation created successfully
